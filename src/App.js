a3bdbe1a6fa379512ccab979b9e79bf32315f276
jamiebdavis <jamied1985@gmail.com>
2019-02-08 16:07:58 +0000

    added container component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Container from "./components/container/container";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h3>Air Quality Tech Test</h3>
+        <Container />
       </div>
     );
   }
