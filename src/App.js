0840cbf36fad9f0ceab999ba2925525d4db540c0
jamiebdavis <jamied1985@gmail.com>
2019-02-10 11:41:57 +0000

    pulling data from api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import Container from "./components/container/container";
+import Container from "./Components/Container";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h3>Air Quality Tech Test</h3>
         <Container />
       </div>
     );
